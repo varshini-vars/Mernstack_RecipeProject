ae8287a86dcdb28698bf7f98853e28a61d314358
varshini-vars <vk9487706969@gmail.com>
2025-03-09 16:14:03 +0530

    Update Register.jsx

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -11,7 +11,7 @@ const Register = ({ setIsLoggedIn }) => {
   const navigate = useNavigate();
 
   const handleRegister = async () => {
-    await axios.post("http://localhost:3000/register", { username, email, password });
+    await axios.post("https://backend-gurr.onrender.com/register", { username, email, password });
     localStorage.setItem("username", username);
     setIsLoggedIn(true);
     navigate("/home");
